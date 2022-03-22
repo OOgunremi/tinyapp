e7c4027fbf1423b0f3c532e6295db302119c3c23
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-22 15:03:10 -0600

    fixed bugs on express_server.js preventing the longURL from showing. Refactored the other codes

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const res = require('express/lib/response');
+//const res = require('express/lib/response');
 const app  = express();
-const port = 1982;
+const port = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 //const templateVars = {urls: urlDatabase};
 const urlDatabase = {
@@ -11,17 +13,29 @@ const urlDatabase = {
 app.get('/', (req, res) => {
   res.send('Hello Folks!');
 });
-app. get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
+//app. get('/urls.json', (req, res) => {
+//  res.json(urlDatabase);
+//});
+//app.get('/hello', (req, res) => {
+//  res.send('<html><body>Hello <b>World</b></body></html>\n');
+//});
 app.get('/urls', (req, res) => {
   res.render('urls_index', {urls: urlDatabase});
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get('/urls/:shortURL', (req, res) => {
-  res.render('urls_show', { shortURL: req.params.shortURL, longURL: req.params.longURL });
+  console.log(req.params);
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = {shortURL, longURL};
+  //console.log(req.url);
+  res.render('urls_show', templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
