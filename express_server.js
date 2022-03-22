0c1263539705155b4ba943166bdb0ec79932179b
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-21 22:21:18 -0600

    added the html format to the ejs file

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const res = require('express/lib/response');
 const app  = express();
 const port = 1982;
 app.set('view engine', 'ejs');
+//const templateVars = {urls: urlDatabase};
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,13 +17,8 @@ app. get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
-app.get('/set', (req, res) => {
-
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-app.get('/fetch', (res, req) => {
-  res.send(`a = ${a}`);
+app.get('/urls', (req, res) => {
+  res.render('urls_index', {urls: urlDatabase});
 });
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
