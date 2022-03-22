f24344656da91d8b636805538e8ec9b6e1317e8d
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-22 16:16:40 -0600

    modified the server.js to include generateRandomString

--- express_server.js
+++ express_server.js
@@ -5,7 +5,10 @@ const port = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-//const templateVars = {urls: urlDatabase};
+function generateRandomString() {
+
+};
+generateRandomString();
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,12 +16,6 @@ const urlDatabase = {
 app.get('/', (req, res) => {
   res.send('Hello Folks!');
 });
-//app. get('/urls.json', (req, res) => {
-//  res.json(urlDatabase);
-//});
-//app.get('/hello', (req, res) => {
-//  res.send('<html><body>Hello <b>World</b></body></html>\n');
-//});
 app.get('/urls', (req, res) => {
   res.render('urls_index', {urls: urlDatabase});
 });
@@ -30,11 +27,10 @@ app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   const templateVars = {shortURL, longURL};
-  //console.log(req.url);
   res.render('urls_show', templateVars);
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log('req.body = ',req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.listen(port, () => {
