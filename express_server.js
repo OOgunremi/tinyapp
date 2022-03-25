378dde9c156ec5652523fffefd3851e45c8097f3
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-24 21:44:31 -0600

    Updated all Basic Permission Features

--- express_server.js
+++ express_server.js
@@ -11,19 +11,28 @@ app.set('view engine', 'ejs');
 
 //DATABASE
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+
+  
+    
+  }
 };
 const users = {
-  "user1": {
+  "user1ID": {
     id: "user1ID",
-    email: "a@b.com",
-    password: "abc"
+    email: "OOgunremi@yahoo.com",
+    password: "123"
   },
   "user2ID": {
     id: "user2ID",
     email: "b@c.com",
-    password: "123"
+    password: "124"
   }
 };
 
@@ -48,33 +57,55 @@ const generateRandomString = (noOfChars) => {
 
 app.get('/urls', (req, res) => {
   const userId = req.cookies['user_id'];
+  if (userId === undefined) {
+    res.redirect(`/login`);
+  }
   const user = users[userId];
+  //function gies here
   const templateVars = {urls: urlDatabase,  user};
+  console.log(user);
+
   res.render('urls_index', templateVars);
 });
 app.get("/urls/new", (req, res) => {
   const userId = req.cookies['user_id'];
+  if (userId === undefined) {
+    res.redirect(`/login`);
+  }
   const user = users[userId];
   const templateVars = {user};
   res.render("urls_new", templateVars);
 });
 app.get('/urls/:shortURL', (req, res) => {
   const userId = req.cookies['user_id'];
+  if (userId === undefined) {
+    res.redirect(`/login`);
+  }
   const user = users[userId];
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
   const templateVars = {user, shortURL, longURL};
   res.render('urls_show', templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
+  if (!longURL) {
+    res.send('Invalid URL. Please check URL Input');
+  }
   res.redirect(longURL);
 });
 app.post("/urls", (req, res) => {
+  const userId = req.cookies['user_id'];
+  if (userId === undefined) {
+    res.redirect(`/login`);
+  }
   const shortURLNew = generateRandomString(6);
   const longURLNew = req.body.longURL;
-  urlDatabase[shortURLNew] = longURLNew;
+  urlDatabase[shortURLNew] = {
+    longURL: longURLNew,
+    userID: userId
+  };
   res.redirect(`/urls/${shortURLNew}`);
 });
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -85,7 +116,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL/edit", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL].longURL = longURL;
   res.redirect(`/urls`);
 });
 app.get("/login", (req, res) => {
@@ -94,21 +125,24 @@ app.get("/login", (req, res) => {
 });
 app.post("/login", (req, res) => {
   const email = req.body.email;
+  console.log('email = ', email);
   const password = req.body.password;
   if (email === '' || password === '') {
-    res.send('Please register/login');
+    res.send('Please enter your email and password and retry');
     return;
   }
   const user = emailLookUp(email);
   if (!user) {
-    res.send('Please check password');
+    console.log('users =', users);
+    res.send('User is not in the database');
+  } else {
+    if (password === user.password) {
+      res.cookie('user_id', user.id);
+      res.redirect(`/urls`);
+    } else {
+      res.send('Please check your password and retry');
+    }
   }
-
-  if (password === user.password) {
-    res.cookie('user_id', user.id);
-    res.redirect(`/urls`);
-  }
-
 });
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
@@ -135,7 +169,6 @@ app.post("/register", (req, res) => {
     password: req.body.password
   };
   users[userId] = user;
-  console.log(users);
   res.cookie('user_id', userId);
   res.redirect(`/urls/`);
 });
